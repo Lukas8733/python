b6f804e3c231849970fa188edf033e1331944501
Lukas8733 <171wita05@o365.htl-leoben.at>
2018-03-05 16:30:23 +0100

    Anfangs und Endzahl

--- zahlenraten.py
+++ zahlenraten.py
@@ -4,8 +4,8 @@ import random
 
 titel = "Willkommen beim Zahlen-Rate-Spiel!"
 print (titel)
-anfang = (input("Bitte die Anfangs Zufallszahl eingeben"))
-ende = (input("Bitte die Anfangs Zufallszahl eingeben"))
+anfang = int(input("Bitte die Anfangs Zufallszahl eingeben"))
+ende = int(input("Bitte die Anfangs Zufallszahl eingeben"))
 
 zufallszahl = (random.randint(anfang,ende))
 text = "Bitte versuche meine Zahl zwischen 1 und 100 zu erraten"
