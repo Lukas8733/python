172abb646e0464ba70a0fd5205de385d52fe822a
Lukas8733 <171wita05@o365.htl-leoben.at>
2018-03-05 16:45:40 +0100

    ForSchleife

---
+++ forSchleife.py
@@ -0,0 +1,14 @@
+#einfache For-Schleife
+
+for i in rage(1,10):
+  print("Hallo",end=", ")
+
+name = "Wilkommen an der HTL Leoben"
+print(name)
+for z in name:
+    print("=", end="")
+print()
+print(name)
+for z in name:
+    print("=", end="") 
+print()
